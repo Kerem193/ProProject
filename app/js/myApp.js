d5b8494932c1c2307b2f53b6447d87c8aa88b3c0
Kerem Uyanik <keremuyanik@wl-1222.wlan.uni-konstanz.de>
2015-04-19 15:41:37 +0200

    adding prototype basis

---
+++ app/js/myApp.js
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+
+var myApp = angular.module('myApp', []);
+
+myApp.controller('appCtrl', function($scope){
+	$scope.xyz = "Hello World";
+	$scope.test="Daten 3"
+		
+		
+		//ruf server auf
+});
+
+/*myApp.controller('tableCtrl', function($scope) {
+	var consultants;
+	$.ajax({
+		url: "http://services.odata.org/V4/Northwind/Northwind.svc/Employees",
+		async: false,
+		dataType: "json"
+	}).done(function(data){
+		$scope.consultants = data.value;
+	});*/
+
+myApp.controller('tableCtrl', function($scope, $http) {
+	$http.get("http://services.odata.org/V4/Northwind/Northwind.svc/Employees").then(function(data){
+		$scope.consultants = data.data.value;
+	});
+});
+
+myApp.controller('serverCtrl', function($scope, $http) {
+	$http.get("localhost:8080/").then(function(data){
+		$scope.consultants = data.data.value;
+	});
+})
